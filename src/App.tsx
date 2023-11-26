fa2fb8cfdb22ce4eb972fd67743761021ff98626
Beshoynady93 <101524188+Beshoynady93@users.noreply.github.com>
2023-11-26 02:17:06 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-primary-red-400 font-primary font-bold-7">Hello</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-blue-200': 'hsl(239, 57%, 85%)',
+        'primary-blue-400': 'hsl(238, 40%, 52%)',
+        'primary-red-200': 'hsl(357, 100%, 86%)',
+        'primary-red-400': 'hsl(358, 79%, 66%)',
+        'neutral-blue-200': 'hsl(211, 10%, 45%)',
+        'neutral-blue-400': 'hsl(212, 24%, 26%)',
+        'neutral-grey-200': 'hsl(228, 33%, 97%)',
+        'neutral-grey-400': 'hsl(223, 19%, 93%)',
+      },
+      fontFamily: {
+        primary: 'Rubik, sans-serif',
+      },
+      fontWeight: {
+        regular: 400,
+        medium: 500,
+        'bold-7': 700,
+      },
+    },
+  },
+  plugins: [],
+};
